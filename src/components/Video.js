de2942e3b75567ef05f6cb3521e87ba44fccbe5e
Tatyana Alekseeva <alekseeva.tatiana.vi@gmail.com>
2023-06-20 11:12:46 +0300

    init: start task

---
+++ src/components/Video.js
@@ -0,0 +1,17 @@
+import DateTime from "./DateTime";
+
+const Video = (props) => {
+  return (
+    <div className="video">
+      <iframe
+        src={props.url}
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+      ></iframe>
+      <DateTime date={props.date} />
+    </div>
+  );
+};
+
+export default Video;
